f44f46f07fa86d22e7c94111c2e64bb4776a4d5e
Abdalle Abdirahman <cabdaleroy19@gmail.com>
2026-02-07 07:19:58 -0800

    i did some upsate

---
+++ srprin.js
@@ -0,0 +1,21 @@
+let cartQuantity = 0;
+
+function showQuantity() {
+  console.log("Cart Quantity: " + cartQuantity);
+}
+
+function addToOne() {
+  cartQuantity += 1;
+}
+
+function addToTwo() {
+  cartQuantity += 2;
+}
+
+function addToThree() {
+  cartQuantity += 3;
+}
+
+function resetCart() {
+  cartQuantity = 0;
+}
