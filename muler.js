835c31be0c739dccc75780a214b9ec4a0c2e4ae9
Abdalle Abdirahman <cabdaleroy19@gmail.com>
2026-02-09 12:28:30 -0800

    update

--- muler.js
+++ muler.js
@@ -1,39 +1,39 @@
 function pickComputerMove() {
-  const randomNumber = Math.random(); // gives a number between 0 and 1
+  const randomNumber = Math.random();
   if (randomNumber < 1/3) return 'rock';
   else if (randomNumber < 2/3) return 'paper';
   else return 'scissors';
 }
-function playGame(playerMove ){
-         const computerMove = pickComputerMove();
-   let result = '';
-    
 
- 
-    if (playerMove==='scissors')
-{
+const score = {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+  let result = '';
+
+  if (playerMove === 'scissors') {
     if (computerMove === 'rock') {
-      result = 'yoi lose';
+      result = 'you lose';
     } else if (computerMove === 'paper') {
       result = 'you win';
-    } else if(computerMove==='Scissors') {
+    } else {
       result = 'Tie';
     }
-  }
-    else if(playerMove==='paper' ){
-
-
+  } 
+  else if (playerMove === 'paper') {
     if (computerMove === 'rock') {
       result = 'you win';
     } else if (computerMove === 'paper') {
       result = 'Tie';
-    } else if(computerMove==='scissors'){
-      result = 'You lose';
+    } else {
+      result = 'you lose';
     }
-    }
-else if(playerMove==='rock'){
-  
-
+  } 
+  else if (playerMove === 'rock') {
     if (computerMove === 'rock') {
       result = 'Tie';
     } else if (computerMove === 'paper') {
@@ -41,12 +41,18 @@ else if(playerMove==='rock'){
     } else {
       result = 'you win';
     }
+  }
+
+  if (result === 'you win') {
+    score.wins += 1;
+  } else if (result === 'you lose') {
+    score.losses += 1;
+  } else {
+    score.ties += 1;
+  }
+
+  alert(
+    `You picked ${playerMove}. Computer picked ${computerMove}.
+Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`
+  );
 }
-
-    alert(
-     `You picked ${playerMove}. Computer picked ${computerMove}. ${result}`
-
-    )
-
-    
-};
